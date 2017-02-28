fb27b06719c2b820ed47b62cbcac3e8dc61799ac
Nicholas Vercruysse <nicholas.vercruysse@gmail.com>
2017-02-28 07:53:25 -0500

    Updated to cover questions 1.3 - 1.6

--- src/ChapterOne/ChapterOne.java
+++ src/ChapterOne/ChapterOne.java
@@ -1,6 +1,8 @@
 package ChapterOne;
 
 import java.util.BitSet;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class ChapterOne {
@@ -10,6 +12,11 @@ public class ChapterOne {
 		System.out.println("Select a function to run: ");
 		System.out.println("1. isUnique: check if a String is comprised only of unique characters");
 		System.out.println("2. checkPermutation: check if two strings are permutations");
+		System.out.println("3. URLify: Replace spaces with %20");
+		System.out.println("4. palindromePermutation: Check if the string is a permutation of a palindrome");
+		System.out.println("5. oneAway: Check if two strings are off by 1 edit");
+		System.out.println("6. compress: compress a string");
+		
 		Scanner in = new Scanner(System.in);
 		int choice = in.nextInt();
 		String garbage = in.nextLine();
@@ -34,9 +41,120 @@ public class ChapterOne {
 			System.out.println("Do you want the permutations to be case sensitive?");
 			String caseSensitive = in.nextLine();
 			System.out.println(checkPermutation(s, s2, numChar, lowestChar, caseSensitive));
+		} else if (choice == 3){
+			System.out.println("Please enter the String to be URLified");
+			System.out.println("Do note that as a part of the assumption in this question,");
+			System.out.println("there have to be 2x as many spaces at the end of the String");
+			System.out.println("as there are within the String");
+			String s = in.nextLine();
+			System.out.println("Enter the true length of the string(i.e. minus the extra spaces");
+			int l = in.nextInt();
+			System.out.println(URLify(s, l));
+		} else if (choice == 4){
+			System.out.println("Please enter the String to be checked");
+			String s = in.nextLine();
+			System.out.println(palindromePermutation(s));
+		} else if (choice == 5) {
+			System.out.println("Please enter first String");
+			String s1 = in.nextLine();
+			System.out.println("Please enter second String");
+			String s2 = in.nextLine();
+			System.out.println(oneAway(s1, s2));
+		} else if (choice == 6){
+			System.out.println("Please enter String to be compressed");
+			String s = in.nextLine();
+			System.out.println(compress(s));
 		} else {
 			System.out.println("No other functions are supported at this time.");
 		}
+	}
+	
+	public static String compress(String s){
+		//question states that only alphabetic chars are allowed
+		StringBuilder sb = new StringBuilder();
+		int i = 0;
+		char c = 'a';
+		while (i < s.length()){
+			int nums = 0;
+			c = s.charAt(i);
+			while (i <s.length() && s.charAt(i) == c){
+				nums++;
+				i++;
+			}
+			sb.append(c);
+			sb.append(nums);
+			
+		}
+		return s.length() > sb.length() ? sb.toString() : s;
+		
+	}
+	public static boolean oneAway(String one, String two){
+		//implement the dynamic programming solution to this for practice later.
+		if (Math.abs(one.length() - two.length()) >= 2)
+			return false;
+		if (one.length() != two.length()){
+			String temp = one.length() < two.length() ? one : two;
+			one = one.length() < two.length() ? two : one;
+			two = temp;
+			int i = 0, j = 0;
+		
+			while (i < one.length() && j < two.length()){
+				if (one.codePointAt(i) != two.codePointAt(j)){
+					i++;
+					continue;
+				}
+				i++;
+				j++;
+			}
+			return i == j + 1;
+		} else {
+			int difs = 0;
+			int i = 0;
+			while (i < one.length()){
+				if (one.codePointAt(i) != two.codePointAt(i)){
+					difs++;
+				}
+				if (difs > 1)
+					return false;
+				i++;
+			}
+			return true;
+		}
+	}
+	
+	public static boolean palindromePermutation(String s){
+		s = s.toLowerCase();
+		HashSet<Integer> set = new HashSet<>();
+		for (int i = 0; i < s.length(); i++){
+			if (s.charAt(i) == ' ')
+				continue;
+			Integer c = s.codePointAt(i);
+			if (set.contains(c))
+				set.remove(c);
+			else
+				set.add(c);
+		}
+		if (set.size() > 1){
+			return false;
+		}
+		return true;
+	}
+	
+	public static String URLify(String s, int trueLength){
+		//question states that we can assume there is enough space at the end of the string
+		//to hold the extra characters needed
+		char[] arr = s.toCharArray();
+		int end = arr.length - 1;
+		for (int i = trueLength - 1; i >= 0; i--){
+			if (arr[i] != ' '){
+				arr[end--] = arr[i];
+			} else {
+				arr[end--] = '0';
+				arr[end--] = '2';
+				arr[end--] = '%';
+			}
+		}
+		return new String(arr);
 	}
 	
 	public static boolean checkPermutation(String one, String two, int numChar, int lowestChar,
